a39dd38d840018c7e4c30db00fe98d3cccaf3259
Alexandre Baudouin Vegas <alexrbvegas@gmail.com>
2023-06-16 09:13:54 -0400

    removed realtime db

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,6 @@ export const environment = {
   firebase: {
     projectId: 'v34l-9d889',
     appId: '1:463007396472:web:2a99bfc449b01b7440f38d',
-    databaseURL: 'https://v34l-9d889-default-rtdb.firebaseio.com',
     storageBucket: 'v34l-9d889.appspot.com',
     apiKey: 'AIzaSyATKYsqH0WFOuN2fPY0OvhH6QrtduNXHeQ',
     authDomain: 'v34l-9d889.firebaseapp.com',
